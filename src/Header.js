ecf5a15ac742eef9db8854737a113772af973806
Midori Yang <myang5@wellesley.edu>
2020-04-03 19:10:48 -0400

    CSS changes

--- src/Header.js
+++ src/Header.js
@@ -8,14 +8,13 @@ function Header() {
       </h1>
       <div className="horizontal">
         <p>
-          A website to more easily upload event/gacha stories from the mobile idol game Ensemble Stars to the fandom wiki.
+          A website to more easily upload event/gacha stories from the mobile idol game DREAM!ing to the fandom wiki.
           <br />It takes formats your story chapter into text that can be pasted directly into the "source"
         section of the page.
           <br />Developed by <a target="_blank" href="https://twitter.com/gayandasleep">midori</a>.
         </p>
         <ul id="navbar">
           <li><a href="./howto.html">HOW TO USE</a></li>
-          <li><a href="./plans.html">DESIGN PLANS</a></li>
           <li><a target="_blank" href="https://goo.gl/forms/Xu42LLAgWKxVYV873">FEEDBACK</a></li>
         </ul>
       </div>
